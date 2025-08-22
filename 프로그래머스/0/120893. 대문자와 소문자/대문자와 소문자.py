733122fa0d0a537d8852c2550afa88eb873436ca
song-dh <sdahye919@gmail.com>
2025-08-23 07:46:55 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.25 ms, Memory: 9.24 MB -BaekjoonHub

---
+++ 프로그래머스/0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    answer = ''
+    for c in my_string:
+        if (c.isupper()):
+            answer += c.lower()
+        else:
+            answer += c.upper()
+    return answer
