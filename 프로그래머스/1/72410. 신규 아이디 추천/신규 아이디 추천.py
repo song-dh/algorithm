413e9c3e6772f35854bcb228d2bf0320d853f810
song-dh <sdahye919@gmail.com>
2025-09-05 02:41:44 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.27 ms, Memory: 9.15 MB -BaekjoonHub

---
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.py
@@ -0,0 +1,22 @@
+def solution(new_id):
+    idList = []
+    for s in new_id:
+        if s.islower() or s.isdigit() or s == '-' or s == '_':
+            idList.append(s)
+        elif s.isupper():
+            idList.append(s.lower())
+        elif s == ".":
+            if idList and idList[len(idList) - 1] != '.':
+                idList.append(s)
+        if len(idList) == 15:
+            break
+            
+    while(idList and idList[len(idList)-1] == '.'):
+        idList.pop()
+        
+    if not idList:
+        idList.append('a')
+    
+    while(len(idList) < 3):
+        idList.append(idList[len(idList)-1])
+    return ''.join(idList)
