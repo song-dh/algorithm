9555ae37a08069ffb9fa7bfec312e185379bde77
song-dh <sdahye919@gmail.com>
2025-09-24 23:13:35 +0900

    [level 2] Title: 전화번호 목록, Time: 851.23 ms, Memory: 241 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -0,0 +1,15 @@
+def solution(phone_book):
+    pb = {}
+    for s in phone_book:
+        p = pb
+        end = len(s) -  1
+        for i, c in enumerate(s):
+            if c in p:
+                if end == i or 'x' in p[c]:
+                    return False
+            else:
+                p[c] = {}
+                if end == i:
+                    p[c] = {'x':''}
+            p = p[c]
+    return True
