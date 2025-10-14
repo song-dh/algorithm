3645298d485d30b131d0d7c08babc28251985acc
song-dh <sdahye919@gmail.com>
2025-10-15 02:51:27 +0900

    [level 2] Title: 피로도, Time: 53.00 ms, Memory: 9.19 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.py
+++ 프로그래머스/2/87946. 피로도/피로도.py
@@ -2,12 +2,11 @@ def solution(k, dungeons):
     visit = [0 for _ in range(len(dungeons))]
     return go(k, dungeons, 0, visit)
 
-def go(k, dungeons, m, visit):
+def go(k, dungeons, cnt, visit):
+    m = cnt
     for i in range(len(dungeons)):
         if k >= dungeons[i][0] and visit[i] == 0:
             visit[i] = 1
-            k -= dungeons[i][1]
-            m = max(go(k, dungeons, m, visit), sum(visit))
+            m = max(go(k - dungeons[i][1], dungeons, cnt + 1, visit), m)
             visit[i] = 0
-            k += dungeons[i][1]
     return m
