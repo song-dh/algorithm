c9fb0667b1f3b04719495f764c59075681fb1cc3
song-dh <sdahye919@gmail.com>
2025-10-24 00:41:48 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 6.89 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.py
@@ -1,34 +1,25 @@
 def solution(n, wires):
     tree = {}
-    
+    answer = n
+
     for wire in wires:
         tree.setdefault(wire[0], []).append(wire[1])
         tree.setdefault(wire[1], []).append(wire[0])
-    answer = n
+    
     for wire in wires:
-        a = set([wire[0]])
-        b = list(tree[wire[0]])
-        b.remove(wire[1])
-        while len(b) != 0:
-            tmp = b.pop(0)  
-            if not tmp in a:
-                a.add(tmp)
-                b = b + tree[tmp]
-        answer = min(answer, abs(n-len(a)-len(a)))
+        treeA = set([wire[0]])
+        leftNode = list(tree[wire[0]])
+        leftNode.remove(wire[1])
+        while len(leftNode) != 0:
+            tmp = leftNode.pop(0)  
+            if not tmp in treeA:
+                treeA.add(tmp)
+                leftNode = leftNode + tree[tmp]
+        treeALen = len(treeA)
+        treeBLen = n - treeALen
+        answer = min(answer, abs(treeALen - treeBLen))
         
     return answer
-
-def dfs(tree, wires, wire):
-    print(tree, wires, wire)
-    a = set([wire[0]])
-    b = list(tree[wire[0]])
-    b.remove(wire[1])
-    while len(b) != 0:
-        tmp = b.pop(0)  
-        if not tmp in a:
-            a.add(tmp)
-            b = b + tree[tmp]
-    return len(a)
             
             
         
